9c05ab5c01eb170692255cae3040f7029001b1e3
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2022-01-21 00:07:35 +0100

    fix(config): add jquery_ui_accordion

--- apps/web/sites/default/settings.php
+++ apps/web/sites/default/settings.php
@@ -704,7 +704,9 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
  * will allow the site to run off of all variants of example.com and
  * example.org, with all subdomains included.
  */
-
+$settings['trusted_host_patterns'] = [
+  '^drupal\.traefik\.me$',
+];
 /**
  * The default list of directories that will be ignored by Drupal's file API.
  *
