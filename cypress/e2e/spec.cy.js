530d4be065baed6ce957ecfaced52aaeaddc9252
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2025-10-16 12:20:56 +0200

    Update config

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://drupal.traefik.me');
+    cy.visit(`https://${Cypress.env('SERVERNAME')}`, {failOnStatusCode: false});
     cy.screenshot('first-page');
-  })
+  });
 })
